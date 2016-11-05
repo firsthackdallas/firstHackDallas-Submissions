d6dafc0ae92fe5d11668c272b54b372f880f735f
kenkoolio <kensoko0l@yahoo.com>
2016-11-05 16:41:30 -0500

    working on sessions

--- Coding_Mojo/Red_Ribbon/app/controllers/sessions_controller.rb
+++ Coding_Mojo/Red_Ribbon/app/controllers/sessions_controller.rb
@@ -1,2 +1,14 @@
 class SessionsController < ApplicationController
+  def create
+    @user = User.find_by(users_params[:email])
+    if @user && @user.authenticate(:password)
+
+    end
+  end
+
+  private
+
+  def users_params
+    params.require(:users).permit(:email, :password)
+  end
 end
