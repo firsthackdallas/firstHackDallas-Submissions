fc68d6d92ab6e5130768afac6e8394f845b8c6d2
krisnotchris <krishenderson@gmail.com>
2016-11-05 20:49:55 -0500

    final push

---
+++ controllers/api.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * GET /api
+ * List of API examples.
+ */
+exports.getApi = (req, res) => {
+  res.render('api/index', {
+    title: 'API Examples'
+  });
+};
+
+
+exports.getFileUpload = (req, res) => {
+  res.render('api/upload', {
+    title: 'File Upload'
+  });
+};
+
+exports.postFileUpload = (req, res) => {
+  req.flash('success', { msg: 'File was uploaded successfully.' });
+  res.redirect('/api/upload');
+};
